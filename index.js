24eaf9743eb232c6ad84997a937f17e4dea4b04a
Mega-Mewthree <Mega-Mewthree@users.noreply.github.com>
2018-07-30 14:08:09 -0700

    Fixed bad code.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const htmlEncode = require("htmlencode").htmlEncode;
 
+const PORT = 8080;
+
 const app = express();
 
 app.disable("x-powered-by");
-app.use(express.urlencoded({limit: "10kb", extended: true}));
 
 app.get("/", (req, res) => {
   let title;
   let description;
-  if (req.params) {
-    if (req.params.title) title = req.params.title;
-    if (req.params.description) description = req.params.description;
+  if (req.query) {
+    if (req.query.title) title = req.query.title;
+    if (req.query.description) description = req.query.description;
   }
   if (!title && !description) {
     title = "A website allowing you to send embeds in Discord."
@@ -20,4 +21,6 @@ app.get("/", (req, res) => {
   res.send(`<!DOCTYPE html><html><head>${title ? `<meta property="og:title" content="${htmlEncode(title)}">` : ""}${description ? `<meta property="og:description" content="${htmlEncode(description)}">` : ""}</head><body></body></html>`);
 });
 
-app.listen(8080);
+app.listen(PORT, () => {
+  console.log(`Started listening on port ${PORT} at ${Date.now()}`);
+});
