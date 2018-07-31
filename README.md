6c25dd9b46461d45afc48b697532d0bee07566b4
Mega-Mewthree <Mega-Mewthree@users.noreply.github.com>
2018-07-30 17:13:27 -0700

    More features

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DiscordSafeEmbed
 A web server that allows embedding in Discord based on URL.
+
+Discord: https://nebula.mooo.info/discord-invite
+Reddit: https://reddit.com/r/Nebula_Devs
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const htmlEncode = require("htmlencode").htmlEncode;
 
-const PORT = 8080;
+const PORT = 8001;
 
 const app = express();
 
@@ -10,15 +10,21 @@ app.disable("x-powered-by");
 app.get("/", (req, res) => {
   let title;
   let description;
+  let image;
+  let siteName;
+  let color;
   if (req.query) {
     if (req.query.title) title = req.query.title;
     if (req.query.description) description = req.query.description;
+    if (req.query.image) description = req.query.image;
+    if (req.query.site_name) siteName = req.query.site_name;
+    if (req.query.color) color = req.query.color;
   }
   if (!title && !description) {
     title = "A website allowing you to send embeds in Discord."
-    description = "Just use the query parameters `title` and `description`.";
+    description = `Just use the query parameters "title" and "description".`;
   }
-  res.send(`<!DOCTYPE html><html><head>${title ? `<meta property="og:title" content="${htmlEncode(title)}">` : ""}${description ? `<meta property="og:description" content="${htmlEncode(description)}">` : ""}</head><body></body></html>`);
+  res.send(`<!DOCTYPE html><html><head>${title ? `<meta property="og:title" content="${htmlEncode(title)}">` : ""}${description ? `<meta property="og:description" content="${htmlEncode(description)}">` : ""}${image ? `<meta property="og:image" content="${htmlEncode(image)}">` : ""}${siteName ? `<meta property="og:site_name" content="${htmlEncode(siteName)}">` : ""}${color ? `<meta name="theme-color" content="#${color}">` : ""}</head><body>Made by <a href="https://github.com/Mega-Mewthree">Mega_Mewthree</a><br><a href="https://github.com/Mega-Mewthree/DiscordSafeEmbed">Source Code</a></body></html>`);
 });
 
 app.listen(PORT, () => {
